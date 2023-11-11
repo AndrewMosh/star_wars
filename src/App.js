5d9abb71a851a0626c879b0abc904a8beda2fb1a
Andrei Mosh <99655206+AndrewMosh@users.noreply.github.com>
2023-11-11 19:27:55 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+import FilmList from "./components/FilmList";
+function App() {
+  return (
+    <div>
+      <h1>Films</h1>
+      <FilmList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/apolloClient.js
@@ -0,0 +1,33 @@
+// apolloClient.js
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+// Replace 'YOUR_GRAPHQL_SERVER_URL' with the actual URL of your GraphQL server
+const GRAPHQL_SERVER_URL =
+  "https://swapi-graphql.netlify.app/.netlify/functions/index";
+
+// Create an HTTP link to your GraphQL server
+const httpLink = createHttpLink({
+  uri: GRAPHQL_SERVER_URL,
+});
+
+// Set up the authentication headers (if needed)
+const authLink = setContext((_, { headers }) => {
+  // You can add authentication tokens or other headers here
+  // For example, if you are using JWT tokens:
+  // const token = localStorage.getItem('token');
+  // return {
+  //   headers: {
+  //     ...headers,
+  //     Authorization: token ? `Bearer ${token}` : '',
+  //   },
+  // };
+});
+
+// Create the Apollo Client instance
+const client = new ApolloClient({
+  link: authLink.concat(httpLink), // Apply the authentication headers (if needed) to the HTTP link
+  cache: new InMemoryCache(),
+});
+
+export default client;
