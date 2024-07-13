52e58ffb87a65668ad35f4e5cf5648f372d57618
Andrew Mosh <malinaenglishclub@gmail.com>
2024-07-13 19:36:35 +0300

    image path fixed

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,10 +1,8 @@
-// apolloClient.js
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 // Replace 'YOUR_GRAPHQL_SERVER_URL' with the actual URL of your GraphQL server
-const GRAPHQL_SERVER_URL =
-    "https://swapi-graphql.netlify.app/.netlify/functions/index";
+const GRAPHQL_SERVER_URL = "https://swapi-graphql.netlify.app/.netlify/functions/index";
 
 // Create an HTTP link to your GraphQL server
 const httpLink = createHttpLink({
@@ -12,16 +10,17 @@ const httpLink = createHttpLink({
 });
 
 // Set up the authentication headers (if needed)
-const authLink = setContext((_, { headers }) => {
+const authLink = setContext((_: any, { headers }: any) => {
     // You can add authentication tokens or other headers here
     // For example, if you are using JWT tokens:
     // const token = localStorage.getItem('token');
     // return {
     //   headers: {
     //     ...headers,
-    //     Authorization: token ? `Bearer ${token}` : '',
+    //     Authorization: token ? Bearer ${token} : '',
     //   },
     // };
+    return { headers: { ...headers } };
 });
 
 // Create the Apollo Client instance
@@ -29,5 +28,6 @@ const client = new ApolloClient({
     link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
 });
+
 
 export default client;
